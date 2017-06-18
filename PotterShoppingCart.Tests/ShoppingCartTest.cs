5a26f705e2b251ac779a32ff9bd17968855e2d49
Owen <owen2862@live.com>
2017-06-18 18:52:34 +0800

    加入專案檔案。

---
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests {
+    /// <summary>
+    /// 第一集買了一本，其他都沒買，價格應為100*1=100元 的摘要說明
+    /// </summary>
+    [TestClass]
+    public class ShoppingCartTest {
+        [TestMethod]
+        public void Buy_first_episode_count_1_should_return_100() {
+
+            //arrange
+            var target = new ShoppingCart();
+            var expected = 100;
+            var firstEpisode = new HarryPotterFirstEpisode() { Price = 100, Count = 1 };
+
+            //act
+            int actual = target.CaculatePrice(new List<Book>() { firstEpisode });
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PotterShoppingCart {
+    public class ShoppingCart {
+        public int CaculatePrice(IEnumerable<Book> books) {
+            int totalPrice = 0;
+            return totalPrice;
+        }
+    }
+
+
+    public abstract class Book {
+        public abstract int Price { get; set; }
+        public abstract int Count { get; set; }
+    }
+
+    public class HarryPotterFirstEpisode : Book {
+        public override int Price { get; set; }
+        public override int Count { get; set; }
+    }
+}
