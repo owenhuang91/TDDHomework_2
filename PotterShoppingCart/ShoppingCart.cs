e46e77efde3785df71cc8559f5940782b4c7bdb2
Owen <owen2862@live.com>
2017-06-21 21:38:53 +0800

    折扣讀取方式改為讀取dictionary

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -5,9 +5,22 @@ using System.Linq;
 
 namespace PotterShoppingCart {
     public class ShoppingCart {
+
+        private Dictionary<int, decimal> discountPercent;
+
+        public ShoppingCart() {
+            discountPercent = new Dictionary<int, decimal>()
+            {
+                {1,1},
+                {2,0.95m},
+                {3,0.9m},
+                {4,0.8m},
+                {5,0.75m}
+            };
+        }
+
         public int CaculatePrice(List<Book> books) {
 
-            int totalPrice = 0;
             decimal rawPrice = 0;
             decimal dicountPercent = 1;
 
@@ -17,42 +30,16 @@ namespace PotterShoppingCart {
                 //找出這一次有哪幾本書可以一起算折扣
                 var needDiscountedBooks = books.Where(m => m.Amount > 0);
 
-                dicountPercent = GetDiscountedPercent(needDiscountedBooks.Count());
+                dicountPercent = discountPercent[needDiscountedBooks.Count()];
                 rawPrice += needDiscountedBooks.Sum(m => m.Price) * dicountPercent;
 
                 //將剛剛已經計算過價格的書扣除
                 books.ForEach(m => { if (m.Amount > 0) m.Amount--; });
             }
 
-            totalPrice = Convert.ToInt32(Math.Round(rawPrice, 0, MidpointRounding.AwayFromZero));
+            int totalPrice = Convert.ToInt32(Math.Round(rawPrice, 0, MidpointRounding.AwayFromZero));
 
             return totalPrice;
-        }
-
-        private decimal GetDiscountedPercent(int booksAmount) {
-
-            decimal percent = 1;
-
-            switch (booksAmount) {
-                case 1:
-                    percent = 1;
-                    break;
-                case 2:
-                    percent = 0.95m;
-                    break;
-                case 3:
-                    percent = 0.9m;
-                    break;
-                case 4:
-                    percent = 0.8m;
-                    break;
-                case 5:
-                    percent = 0.75m;
-                    break;
-                default:
-                    break;
-            }
-            return percent;
         }
     }
 
